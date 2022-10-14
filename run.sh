6ec488f52f396d00cf1a9b95a4691458ab4e69c3
jingslunt <jingslunt@qq.com>
2022-10-14 12:11:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,9 +17,15 @@ chmod +x ./hola-proxy
 echo "run hola"
 nohup ./hola-proxy -country jp > /dev/null 2>&1 &
 
+
 echo "shadowsocks"
 #export HTTPS_PROXY=http://127.0.0.1:8080
 #export HTTP_PROXY=http://127.0.0.1:8080
 #export NO_PROXY=.hola.org,hola.org,.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16
+
+curl -o graftcp-0.4.0-1-x86_64.pkg.tar.zst -L https://github.com/hmgle/graftcp/releases/download/v0.4.0/graftcp-0.4.0-1-x86_64.pkg.tar.zst
+zstd -d graftcp-0.4.0-1-x86_64.pkg.tar.zst
+tar -xvf graftcp-0.4.0-1-x86_64.pkg.tar
+
 
 usr/bin/mgraftcp  --http_proxy=127.0.0.1:8080 go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:'"${password}"'@:8488' -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
