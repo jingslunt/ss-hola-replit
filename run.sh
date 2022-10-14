531e91cf8abe115f6a865e3f061b0ca080a8fab0
jingslunt <jingslunt@qq.com>
2022-10-14 10:52:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,8 +16,8 @@ curl -o hola-proxy -L https://github.com/Snawoot/hola-proxy/releases/download/v1
 chmod +x ./hola-proxy
 nohup ./hola-proxy -country jp > /dev/null 2>&1 &
 
-Environment="HTTP_PROXY=http://127.0.0.1:8080"
-Environment="HTTPS_PROXY=http://127.0.0.1:8080"
-Environment="NO_PROXY=*.hola.org,hola.org,*.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16"
+export HTTPS_PROXY=http://127.0.0.1:8080
+export HTTP_PROXY=http://127.0.0.1:8080
+export NO_PROXY=.hola.org,hola.org,.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16
 
 go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:'"${password}"'@:8488' -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
