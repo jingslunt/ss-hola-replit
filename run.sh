05aa0f28541df9b35827507b0d4fd7b6e825d680
jingslunt <jingslunt@qq.com>
2022-10-14 13:24:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,9 +23,12 @@ echo "shadowsocks"
 #export HTTP_PROXY=http://127.0.0.1:8080
 #export NO_PROXY=.hola.org,hola.org,.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16
 
-curl -o graftcp-0.4.0-1-x86_64.pkg.tar.zst -L https://github.com/hmgle/graftcp/releases/download/v0.4.0/graftcp-0.4.0-1-x86_64.pkg.tar.zst
-zstd -d graftcp-0.4.0-1-x86_64.pkg.tar.zst
-tar -xvf graftcp-0.4.0-1-x86_64.pkg.tar
-
+#curl -o graftcp-0.4.0-1-x86_64.pkg.tar.zst -L https://github.com/hmgle/graftcp/releases/download/v0.4.0/graftcp-0.4.0-1-x86_64.pkg.tar.zst
+#zstd -d graftcp-0.4.0-1-x86_64.pkg.tar.zst
+#tar -xvf graftcp-0.4.0-1-x86_64.pkg.tar
+git clone https://github.com/hmgle/graftcp.git
+cd graftcp
+make
+cd ..
 
 usr/bin/mgraftcp  --http_proxy=127.0.0.1:8080 go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:'"${password}"'@:8488' -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
