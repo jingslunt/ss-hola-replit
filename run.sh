e33bf22a5ab62f73f4129017199781bd98de7b73
jingslunt <jingslunt@qq.com>
2022-10-14 11:06:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,8 +14,10 @@ echo '############################'
 uname -a
 curl -o hola-proxy -L https://github.com/Snawoot/hola-proxy/releases/download/v1.5.4/hola-proxy.linux-amd64
 chmod +x ./hola-proxy
+echo "run hola"
 nohup ./hola-proxy -country jp > /dev/null 2>&1 &
 
+echo "shadowsocks"
 export HTTPS_PROXY=http://127.0.0.1:8080
 export HTTP_PROXY=http://127.0.0.1:8080
 export NO_PROXY=.hola.org,hola.org,.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16
