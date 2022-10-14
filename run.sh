5da2ba317d795430e1671607d1ec2374e7fedffd
jingslunt <jingslunt@qq.com>
2022-10-14 10:44:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,5 +18,6 @@ nohup ./hola-proxy -country jp > /dev/null 2>&1 &
 
 Environment="HTTP_PROXY=http://127.0.0.1:8080"
 Environment="HTTPS_PROXY=http://127.0.0.1:8080"
-Environment="NO_PROXY=*.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16"
+Environment="NO_PROXY=*.hola.org,hola.org,*.local,local,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/16,172.20.0.0/16,192.168.0.0/16"
+
 go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:'"${password}"'@:8488' -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
