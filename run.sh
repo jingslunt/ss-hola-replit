726d5f075fab4b881b86c3a602f3bd97d21997ae
jingslunt <jingslunt@qq.com>
2022-10-14 10:40:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,8 @@ echo $'SS URL:\t\t'"${url}"
 echo 'Also QR code with server credentials is available in this REPL files.'
 echo '############################'
 
-exec go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:'"${password}"'@:8488' -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
+uname -a
+curl -o hola-proxy -L https://github.com/Snawoot/hola-proxy/releases/download/v1.5.4/hola-proxy.linux-amd64
+chmod +x ./hola-proxy
+nohup ./hola-proxy -country jp > /dev/null 2>&1 &
+go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:'"${password}"'@:8488' -verbose -plugin v2ray-plugin -plugin-opts "server" -udp=false
