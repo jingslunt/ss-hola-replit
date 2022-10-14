f99e4516479990e450465766426530a82fd695fd
jingslunt <jingslunt@qq.com>
2022-10-14 14:24:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ curl -o hola-proxy -L https://github.com/Snawoot/hola-proxy/releases/download/v1
 chmod +x ./hola-proxy
 echo "run hola"
 fi
-ps -ef |grep hola-proxy|grep -v >/dev/null || nohup ./hola-proxy -bind-address 127.0.0.1:8888 -country jp > /dev/null 2>&1 &
+ps -ef |grep hola-proxy|grep -v grep >/dev/null || nohup ./hola-proxy -bind-address 127.0.0.1:8888 -country jp > /dev/null 2>&1 &
 
 
 echo "shadowsocks"
